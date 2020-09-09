141f92edd8917b891452c949b05aed914684fae2
lprivette <lprivette@usgs.gov>
2020-09-09 16:35:02 -0400

    allows cycle 3 year to be shown if date is 2017

--- src/scripts/core.js
+++ src/scripts/core.js
@@ -1103,7 +1103,7 @@ require([
                             cycle2 = "NA"
                         }
                         
-                        if ((attr["tbl_Networks.Cycle3_SampleFY"] != "") && (attr["tbl_Networks.Cycle3_SampleFY"] < 2017)) {
+                        if (attr["tbl_Networks.Cycle3_SampleFY"] != "") {
                             cycle3 = attr["tbl_Networks.Cycle3_SampleFY"];
                             if (attr["tbl_Networks.Cycle3_SampleFY"] == null) {
                                 cycle3 = "NA"
